e1db898a317085160a8441d15c9fa8c3502ba6ca
Firoz Shaikh <gitfiroz695@gmail.com>
2023-05-26 16:08:33 +0530

    Handling Pagination with Retrofit

--- app/src/main/java/com/androiddevs/mvvmnewsapp/models/NewsResponse.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/NewsResponse.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.androiddevs.mvvmnewsapp.models.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
--- app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -6,5 +6,6 @@ class Constants {
         const val API_KEY = "3dfd1fe7b3114e09a88785f2e61cd139"
         const val BASE_URL = "https//newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
